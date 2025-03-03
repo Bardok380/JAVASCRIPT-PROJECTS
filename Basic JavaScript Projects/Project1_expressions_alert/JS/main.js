260272513b8fc03f6c1e4305a7b5dfea66e531da
Bardok380 <rcamadorclements@gmail.com>
2025-03-03 14:08:06 -0800

    First Commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,6 @@
+window.alert("My first JS assignment"); //Will create a small window when executed in the browser
+var Sent1 = "This is the beginning of the string";
+var Sent2 = "\ and this is the end of the string";
+document.write(Sent1 + Sent2) //Concatenate of the variables Sent1 and Sent2
+document.write("\n /"); // Space added between previous Document.write and the new one. The "/" indicates a seperation between the two lines of code.
+document.write(6*8); // Expression of 6 multiplied by 8
